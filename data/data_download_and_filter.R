5e65632f66fbef09fc66d24adea4d64d394b3985
Stefan Milosavljevic <supermegaiperste@hotmail.com>
2020-03-04 11:41:21 +0100

    Creating new scripts which include just the code used for the analyses.

---
+++ data/data_download_and_filter.R
@@ -0,0 +1,40 @@
+## Script to download the data used for the benchmark,
+## for more details check data_download_and_filter.html
+
+## Load required libraries
+
+library(GEOquery)
+
+## Get files
+
+sfiles <- getGEOSuppFiles("GSE64177", makeDirectory = F)
+
+# Extract the "Not Infected (NI)" files from `tar` and remove the `tar` file
+
+system("tar -xvf GSE64177_RAW.tar  --wildcards --no-anchored '*NI*' && rm -rf GSE64177_RAW.tar")
+
+# Rename data and move it to new folder called raw
+
+files <- list.files(path = ".", pattern = ".gz", full.names = T)
+
+tmp <- lapply(files, function(x) {
+  n <- paste0(strsplit(basename(x), "\\.")[[1]][1], ".chr18.txt.gz")
+  system(paste(
+    "zcat", x, "| grep chr18 | pigz -11 -p 16 >", n,
+    "&& rm -rf", x,
+    "&& mkdir -p raw && mv *.chr18* ./raw/"
+  ))
+})
+
+# Save dataframe with information about the samples 
+
+raw.files <- list.files(path = "./raw", pattern = ".gz", full.names = T)
+pData <- data.frame(files = basename(raw.files), 
+                    files_sl = paste0("NC_rep", 1:length(raw.files), ".bed.gz"),
+                    names = paste0("NC_rep", 1:length(raw.files)),
+                    Group = rep(c("Group1", "Group2"), (length(raw.files)/2)),
+                    stringsAsFactors = F)
+
+rownames(pData) <- pData$names
+
+write.table(pData, gzfile("phenoData.txt.gz"), quote = F, sep = "\t")
--- rules/data_download.smk
+++ rules/data_download.smk
@@ -2,14 +2,18 @@
 
 rule dmrseq_data_download:
     input:
-        data + "data_download_and_filter.Rmd"
+        data + "data_download_and_filter.R"
     output:
-        data + "data_download_and_filter.html"
+        data + "raw/GSM1565940_DC81_NI_5mC.chr18.txt.gz",
+        data + "raw/GSM1565942_DC82_NI_5mC.chr18.txt.gz",
+        data + "raw/GSM1565944_DC83_NI_5mC.chr18.txt.gz",
+        data + "raw/GSM1565946_DC87_NI_5mC.chr18.txt.gz",
+        data + "raw/GSM1565948_DC89_NI_5mC.chr18.txt.gz",
+        data + "raw/GSM1565950_DC91_NI_5mC.chr18.txt.gz",
+        data + "phenoData.txt.gz"
     log:
         data + "log"
     conda:
         "../envs/environment_R.yaml"
     shell:
-        """
-        Rscript -e 'rmarkdown::render("{input}", "html_document")' 2> {log} >> {log}
-        """
+        "Rscript {input} 2> {log} >> {log}"
